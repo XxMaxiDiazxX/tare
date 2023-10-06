65ba5dd3388f9a5290778660691a19ea10a30ac2
XxMaxiDiazxX <johansebastianvelezortiz@gmail.com>
2023-10-06 17:26:49 -0500

    read

--- Js/index.js
+++ Js/index.js
@@ -3,7 +3,8 @@ import { CRUD } from "./CRUD.js";
 function app() {
     let crud = new CRUD("ejemplo");
     crud.create([1, 2, 3]);
-
+    crud.create({ message: "hola mundo" });
+    crud.read(3);
 
     let crud2 = new CRUD("ejemplo");
     console.log(crud2.readAll());
