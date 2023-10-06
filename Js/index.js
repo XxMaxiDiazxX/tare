d084304b52bfefb993fc4b41592fec060b4bf428
XxMaxiDiazxX <johansebastianvelezortiz@gmail.com>
2023-10-06 17:27:09 -0500

    read

--- Js/index.js
+++ Js/index.js
@@ -4,6 +4,7 @@ function app() {
     let crud = new CRUD("ejemplo");
     crud.create([1, 2, 3]);
     crud.create({ message: "hola mundo" });
+    crud.update(1, "hola mundo2");
     crud.read(3);
 
     let crud2 = new CRUD("ejemplo");
