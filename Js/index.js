990ddb4ec30ca2b8637c6b9286de3ff1a276246e
XxMaxiDiazxX <johansebastianvelezortiz@gmail.com>
2023-10-06 17:27:36 -0500

    delete

--- Js/index.js
+++ Js/index.js
@@ -5,6 +5,7 @@ function app() {
     crud.create([1, 2, 3]);
     crud.create({ message: "hola mundo" });
     crud.update(1, "hola mundo2");
+    crud.delete(1);
     crud.read(3);
 
     let crud2 = new CRUD("ejemplo");
